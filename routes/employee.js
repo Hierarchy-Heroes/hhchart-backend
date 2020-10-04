0f97c88f81022024731bebc085624106d49f68f4
Kashish Arora <kashisharora99@gmail.com>
2020-10-04 14:13:31 -0400

    Pass in company as collection name when getting employees/creating a new Employee

--- routes/employee.js
+++ routes/employee.js
@@ -1,34 +1,38 @@
-
 const express = require('express');
 const router = express.Router();
-const Employee = require('../models/Employee');
 
-router.get('/', async (req, res) => {
+router.get('/:company', async (req, res) => {
     try {
-        const employees = await Employee.find(); 
+        //TODO: modify collection name to get the company from the currently logged in user
+        //pass in collection name (in this case, the company)
+        const Employee = require('../models/Employee')(req.params.company);
+        const employees = await Employee.find();
         res.json(employees);
     } catch (err) {
         res.json({ message : err });
     }
 })
 
-router.get('/e1', (req, res) => {
+router.get('/employee/:employeeId', (req, res) => {
     res.send("We are looking at employee 1");
 });
 
 /* create a new employee */
 router.post('/', async (req, res) => {
+    //TODO: modify collection name to get the company from the currently logged in user
+    //pass in collection name (in this case, the company)
+    const Employee = require('../models/Employee')(req.body.companyName.replace(/\s/g,''));
     const newEmployee = new Employee({
-        firstName: req.body.firstName, 
-        lastName: req.body.lastName, 
-        companyId: req.body.companyId, 
-        password: req.body.password, 
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        companyId: req.body.companyId,
+        password: req.body.password,
         positionTitle: req.body.positionTitle,
-        companyName: req.body.companyName, 
-        isManager: req.body.isManager, 
-        employeeId: req.body.employeeId, 
-        email: req.body.email, 
-        startDate: req.body.startDate, 
+        companyName: req.body.companyName,
+        isManager: req.body.isManager,
+        employeeId: req.body.employeeId,
+        email: req.body.email,
+        startDate: req.body.startDate,
     });
 
     try {
@@ -39,4 +43,4 @@ router.post('/', async (req, res) => {
     }
 })
 
-module.exports = router; 
+module.exports = router;
