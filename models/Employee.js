fe877b36fee264fa2ef1c9bd9d1060926df858c9
Kashish Arora <kashisharora99@gmail.com>
2020-10-04 14:12:44 -0400

    Create collection paramater for Employee model

--- models/Employee.js
+++ models/Employee.js
@@ -1,30 +1,32 @@
 
 const mongoose = require('mongoose');
-const helpers = require('../helper'); 
+const helpers = require('../helper');
 
 const EmployeeSchema = mongoose.Schema({
     firstName: {
-        type: String, 
-        required: true, 
-    }, 
+        type: String,
+        required: true,
+    },
     lastName: {
         type: String,
-        required: true, 
-    }, 
+        required: true,
+    },
     companyId: Number,
-    password: String, 
-    positionTitle: String, 
-    companyName: String, 
-    isManager: Boolean, 
-    employeeId: Number, 
+    password: String,
+    positionTitle: String,
+    companyName: String,
+    isManager: Boolean,
+    employeeId: Number,
     email: {
-        type: String, 
-        required: true, 
-    }, 
+        type: String,
+        required: true,
+    },
     startDate: {
-        type: String, 
-        default: helpers.formatDate(new Date()), 
+        type: String,
+        default: helpers.formatDate(new Date()),
     }
 });
 
-module.exports = mongoose.model('Employee', EmployeeSchema); 
+module.exports = function(collectionName) {
+    return mongoose.model('Employee', EmployeeSchema, collectionName); ;
+};
