f0fd764c35f1614d2c2e22c75c805f2d07a4ff23
Julian Ventura <julian.ventura20@gmail.com>
2020-11-17 14:06:01 -0500

    Sends email if forgot password

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 
+const nodemailer = require('nodemailer');
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const { validateLogin, emailInUse, matchPassword } = require('../validation');
+const { validateLogin, emailInUse} = require('../validation');
 const { trimSpaces } = require('../misc/helper');
+
 
 router.post('/login', async (req, res) => {
     const { error } = validateLogin(req.body);
@@ -27,10 +29,38 @@ router.post('/login', async (req, res) => {
         _id: user._id
     };
 
+    sendEmail();
+
     // create and assign token to current user 
     const token = jwt.sign(userSignature, process.env.JWT_SECRET, { expiresIn: "2h" });
     res.header('auth-token', token).send(token);
 });
+
+function sendEmail() 
+{
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'testingorgchart@gmail.com',
+          pass: 'pa33word'
+        }
+      });
+
+    var mailOptions = {
+    from: 'testingorgchart@gmail.com',
+    to: 'venturanotes@gmail.com',
+    subject: 'Whud up my brother from another mother',
+    text: 'Hello'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
 
 router.post('/register', async (req, res) => {
     // TODO 
